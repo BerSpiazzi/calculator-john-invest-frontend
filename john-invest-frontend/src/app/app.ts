e981c6e0c2995047ab7111f6a06a0143c323c5af
Bernardo Bortot Spiazzi <bernardo.spiazzi@ciss.com.br>
2025-07-29 16:38:30 -0300

    Ajustes peridiocidade

--- john-invest-frontend/src/app/app.ts
+++ john-invest-frontend/src/app/app.ts
@@ -2,12 +2,14 @@ import {Component, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {DecimalPipe} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
   imports: [
     DecimalPipe,
-    FormsModule
+    FormsModule,
+    MatButtonToggleModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
@@ -19,18 +21,24 @@ export class App {
   aporteMensal = 0;
   taxaJuros = 0;
   tempo = 0;
+  periodicidade = 'mensal';
   resultados: any[] = [];
 
   constructor(private http: HttpClient) {
   }
 
   calcular() {
+    let tempoConvertido = this.tempo;
+
+    if (this.periodicidade === 'anual') {
+      tempoConvertido = this.tempo * 12;
+    }
+
     const payload = {
       valorInicial: this.valorInicial,
       aporteMensal: this.aporteMensal,
       taxaJuros: this.taxaJuros,
-      tempo: this.tempo,
-      periodicidade: 'mensal'
+      tempo: tempoConvertido
     };
 
     this.http.post<any[]>('http://localhost:8080/calcular', payload).subscribe(data => {
